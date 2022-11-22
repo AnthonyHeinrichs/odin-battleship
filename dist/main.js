3d79cc512529d75c907ef6fa761fd70f78714874
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-11-22 17:01:59 +0100

    Added tests for Ship functionality, and added functions

--- dist/main.js
+++ dist/main.js
@@ -13,20 +13,44 @@
 /*!*********************!*\
   !*** ./src/main.js ***!
   \*********************/
-/***/ (() => {
+/***/ ((module) => {
 
-eval("\n\n//# sourceURL=webpack://odin-battleship/./src/main.js?");
+eval("var Ship = function Ship(length) {\n  var sunk = false;\n  var hits = 0;\n  return {\n    length: length,\n    hits: hits,\n    isSunk: sunk\n  };\n};\nvar checkIfSunk = function checkIfSunk(ship) {\n  if (ship.hits >= ship.length) {\n    ship.isSunk = true;\n  }\n};\nvar hitShip = function hitShip(ship) {\n  ship.hits = ship.hits += 1;\n  checkIfSunk(ship);\n};\nmodule.exports = {\n  Ship: Ship,\n  checkIfSunk: checkIfSunk,\n  hitShip: hitShip\n};\n\n//# sourceURL=webpack://odin-battleship/./src/main.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/main.js"]();
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/main.js");
 /******/ 	
 /******/ })()
 ;

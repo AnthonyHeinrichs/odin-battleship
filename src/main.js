7d321250ff7add908dcf99dd48c8709b1612f83c
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-11-24 15:52:06 +0100

    Added title to title screen with animations

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+import './global-styles.css'
 const Gameboard = require('./modules/gameboard')
+
+const mainDiv = document.getElementById('mainDiv')
+
+const battleshipTitle = document.createElement('h1')
+battleshipTitle.classList.add('title')
+battleshipTitle.classList.add('linear-wipe')
+battleshipTitle.innerText = 'Battleship'
+mainDiv.appendChild(battleshipTitle)

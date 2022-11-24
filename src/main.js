e38063337405fc6d1e5f203c7a77428fea98a65c
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-11-24 20:20:45 +0100

    Added gameboard grid and squares

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './global-styles.css'
 import './styles/start-btn.css'
+import './styles/gameboard.css'
 
 const Gameboard = require('./modules/gameboard')
 
@@ -31,15 +32,20 @@ startButton.addEventListener('click', () => {
   computerGameboardDiv.classList.remove('hidden')
 })
 
+const gameboards = document.createElement('div')
+gameboards.classList.add('gameboards')
+mainDiv.appendChild(gameboards)
+
 const playerGameboard = Gameboard()
 
 const playerGameboardDiv = document.createElement('div')
 playerGameboardDiv.classList.add('playerGameboard')
 playerGameboardDiv.classList.add('hidden')
-mainDiv.appendChild(playerGameboardDiv)
+gameboards.appendChild(playerGameboardDiv)
 
 for (let i = 0; i < playerGameboard.board.length; i++) {
   const square = document.createElement('div')
+  square.classList.add('playerSquare')
   square.id = playerGameboard.board[i].id
   playerGameboardDiv.appendChild(square)
 }
@@ -49,10 +55,11 @@ const computerGameboard = Gameboard()
 const computerGameboardDiv = document.createElement('div')
 computerGameboardDiv.classList.add('computerGameboard')
 computerGameboardDiv.classList.add('hidden')
-mainDiv.appendChild(computerGameboardDiv)
+gameboards.appendChild(computerGameboardDiv)
 
 for (let i = 0; i < computerGameboard.board.length; i++) {
   const square = document.createElement('div')
+  square.classList.add('computerSquare')
   square.id = computerGameboard.board[i].id
   computerGameboardDiv.appendChild(square)
 }

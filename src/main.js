6c15ff2bdfb02fd89e8a6bd8158b7e0c7f081cfe
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-11-24 16:51:02 +0100

    Added start button with animations

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import './global-styles.css'
+import './styles/start-btn.css'
+
 const Gameboard = require('./modules/gameboard')
 
 const mainDiv = document.getElementById('mainDiv')
@@ -8,3 +10,12 @@ battleshipTitle.classList.add('title')
 battleshipTitle.classList.add('linear-wipe')
 battleshipTitle.innerText = 'Battleship'
 mainDiv.appendChild(battleshipTitle)
+
+const startBtnDiv = document.createElement('div')
+startBtnDiv.classList.add('center')
+mainDiv.appendChild(startBtnDiv)
+
+const startButton = document.createElement('button')
+startButton.id = 'start'
+startButton.innerText = 'Start Game'
+startBtnDiv.appendChild(startButton)

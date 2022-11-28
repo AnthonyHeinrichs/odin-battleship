98a84bb2de4d714ad7ac5cb61566f63c0524a4f5
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-11-28 13:20:12 +0100

    Updated formatting and added some comments

--- src/components/initial-page-load.js
+++ src/components/initial-page-load.js
@@ -1,9 +1,8 @@
-const initialPageLoad = () => {
+const initialPageLoad = (playerGameboard, computerGameboard) => {
   const mainDiv = document.getElementById("mainDiv");
 
   const titleScreen = document.createElement("div");
   titleScreen.classList.add("titleScreen");
-  titleScreen.classList.add("hidden")
   titleScreen.id = "titleScreen";
   mainDiv.appendChild(titleScreen);
 
@@ -21,6 +20,38 @@ const initialPageLoad = () => {
   startButton.id = "start";
   startButton.innerText = "Start Game";
   startBtnDiv.appendChild(startButton);
+
+  // Load in the gameboards, one for the user & one for the computer
+  const gameboards = document.createElement("div");
+  gameboards.classList.add("gameboards");
+  mainDiv.appendChild(gameboards);
+
+  const playerGameboardDiv = document.createElement("div");
+  playerGameboardDiv.classList.add("playerGameboard");
+  playerGameboardDiv.id = "playerGameboard";
+  playerGameboardDiv.classList.add("hidden");
+  gameboards.appendChild(playerGameboardDiv);
+
+  // Creating a div for each square in the gameboard (10 x 10)
+  for (let i = 0; i < playerGameboard.board.length; i++) {
+    const square = document.createElement("div");
+    square.classList.add("playerSquare");
+    square.id = playerGameboard.board[i].id;
+    playerGameboardDiv.appendChild(square);
+  }
+
+  const computerGameboardDiv = document.createElement("div");
+  computerGameboardDiv.classList.add("computerGameboard");
+  computerGameboardDiv.id = "computerGameboard";
+  computerGameboardDiv.classList.add("hidden");
+  gameboards.appendChild(computerGameboardDiv);
+  // Creating a a div for each square in the gameboard (10 x 10)
+  for (let i = 0; i < computerGameboard.board.length; i++) {
+    const square = document.createElement("div");
+    square.classList.add("computerSquare");
+    square.id = computerGameboard.board[i].id;
+    computerGameboardDiv.appendChild(square);
+  }
 };
 
 export default initialPageLoad;
--- src/main.js
+++ src/main.js
@@ -5,62 +5,39 @@ import initialPageLoad from "./components/initial-page-load";
 import placeShips from "./components/place-ships";
 const Gameboard = require("./modules/gameboard");
 
-initialPageLoad();
-
-
-const startButton = document.getElementById("start");
-const titleScreen = document.getElementById("titleScreen");
-
-// Remove title content when 'Start Game' is selected
-startButton.addEventListener("click", () => {
-  titleScreen.classList.add("hidden");
-  playerGameboardDiv.classList.remove("hidden");
-  computerGameboardDiv.classList.remove("hidden");
-  // runGame()
-});
-
-// Load in the gameboards, one for the user & one for the computer
-const gameboards = document.createElement("div");
-gameboards.classList.add("gameboards");
-mainDiv.appendChild(gameboards);
 // Initializing player gameboard
 const playerGameboard = Gameboard();
-
-const playerGameboardDiv = document.createElement("div");
-playerGameboardDiv.classList.add("playerGameboard");
-// playerGameboardDiv.classList.add("hidden");
-gameboards.appendChild(playerGameboardDiv);
-
-// Creating a div for each square in the gameboard (10 x 10)
-for (let i = 0; i < playerGameboard.board.length; i++) {
-  const square = document.createElement("div");
-  square.classList.add("playerSquare");
-  square.id = playerGameboard.board[i].id;
-  playerGameboardDiv.appendChild(square);
-}
 // Initializing computer gameboard
 const computerGameboard = Gameboard();
 
-const computerGameboardDiv = document.createElement("div");
-computerGameboardDiv.classList.add("computerGameboard");
-// computerGameboardDiv.classList.add("hidden");
-gameboards.appendChild(computerGameboardDiv);
-// Creating a a div for each square in the gameboard (10 x 10)
-for (let i = 0; i < computerGameboard.board.length; i++) {
-  const square = document.createElement("div");
-  square.classList.add("computerSquare");
-  square.id = computerGameboard.board[i].id;
-  computerGameboardDiv.appendChild(square);
-}
+// Executing initial page load to load all DOM elements
+initialPageLoad(playerGameboard, computerGameboard);
+const startButton = document.getElementById("start");
 
-const runGame = () => {
-  // Place ships for user 
-  placeShips(playerGameboard, true)
+// Remove title content when 'Start Game' is selected
+startButton.addEventListener("click", () => {
+  const titleScreen = document.getElementById("titleScreen");
+  const playerGameboardDiv = document.getElementById("playerGameboard");
+  const computerGameboardDiv = document.getElementById("computerGameboard");
+
+  titleScreen.classList.add("hidden");
+  playerGameboardDiv.classList.remove("hidden");
+  computerGameboardDiv.classList.remove("hidden");
+  addShipsToBoard();
+});
+
+// Calling place ships function and passing the gameboards
+const addShipsToBoard = () => {
   // Place ships for computer
-  placeShips(computerGameboard, false)
-}
+  placeShips(computerGameboard, false);
+  // Place ships for user with callback to know when all ships are placed
+  placeShips(playerGameboard, true, startGame);
+};
 
-runGame();
+// Just temporarily checking game ready callback
+function startGame() {
+  console.log("Game ready");
+}
 
 // Event listeners for each square
 

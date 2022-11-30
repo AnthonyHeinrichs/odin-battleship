fc8968675a7ae1f6cba547386009e051138a3faf
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-11-30 02:52:34 +0100

    Setup the AI to play against the user
    1. I wanted the AI to be smart so if it hit a ship I make it so
    it checks the surrounding squares, and continues in a direction if
    it continues to hit.
    
    2. I could probably make the ai a lot better. For example, if the
    ai hits a ship going left, it will try left again, until it misses. The
    problem is the ai doesn't know when it hits a new row. I'm ok with this.
      - ex. ai hits at B1, will try A10 next.
    
    3. There is some temporary logic to reset game, which I will update
    tomorrow with the styling.

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,4 +1,13 @@
 const Player = (id, ifAi) => {
+  let aiHit = false;
+  let hitLeft = false;
+  let hitRight = false;
+  let hitBot = false;
+  let hitTop = false;
+  let firstHitNum = null;
+  let lastHitNum = null;
+  let nums = Array.from({ length: 100 }, (_, i) => i + 1);
+
   let isTurn = false;
   const playerId = id;
   const ai = ifAi;
@@ -9,6 +18,212 @@ const Player = (id, ifAi) => {
     ai: ai,
     setTurn() {
       this.isTurn = !this.isTurn;
+    },
+    aiAttack(gameboard, playerOne, playerTwo, showAttacks, checkWinner) {
+      if (aiHit == false) {
+        let ranNum = Math.floor(Math.random() * nums.length);
+        if (gameboard.receiveAttack(nums[ranNum]) == "Ship hit") {
+          aiHit = true;
+          checkWinner();
+        }
+        lastHitNum = nums[ranNum];
+        firstHitNum = nums[ranNum];
+        nums.splice(ranNum, 1);
+        showAttacks(playerTwo, gameboard);
+      } else if (aiHit == true) {
+        if (hitLeft == true) {
+          if (
+            gameboard.board[lastHitNum - 2] !== undefined &&
+            !gameboard.board[lastHitNum - 2].hasBeenAttacked
+          ) {
+            if (gameboard.receiveAttack(lastHitNum - 1) == "Ship hit") {
+              lastHitNum = lastHitNum - 1;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitLeft = true;
+              showAttacks(playerTwo, gameboard);
+              checkWinner();
+            } else {
+              lastHitNum = lastHitNum - 1;
+              nums = nums.filter((num) => num !== lastHitNum - 1);
+              hitLeft = false;
+              showAttacks(playerTwo, gameboard);
+            }
+          } else {
+            hitLeft = false;
+            this.aiAttack(
+              gameboard,
+              playerOne,
+              playerTwo,
+              showAttacks,
+              checkWinner
+            );
+          }
+        } else if (hitRight == true) {
+          if (
+            gameboard.board[lastHitNum] !== undefined &&
+            !gameboard.board[lastHitNum].hasBeenAttacked
+          ) {
+            if (gameboard.receiveAttack(lastHitNum + 1) == "Ship hit") {
+              lastHitNum = lastHitNum + 1;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitRight = true;
+              showAttacks(playerTwo, gameboard);
+              checkWinner();
+            } else {
+              lastHitNum = lastHitNum + 1;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitRight = false;
+              showAttacks(playerTwo, gameboard);
+            }
+          } else {
+            hitRight = false;
+            this.aiAttack(
+              gameboard,
+              playerOne,
+              playerTwo,
+              showAttacks,
+              checkWinner
+            );
+          }
+        } else if (hitTop == true) {
+          if (
+            gameboard.board[lastHitNum - 11] !== undefined &&
+            !gameboard.board[lastHitNum - 11].hasBeenAttacked
+          ) {
+            if (gameboard.receiveAttack(lastHitNum - 10) == "Ship hit") {
+              lastHitNum = lastHitNum - 10;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitTop = true;
+              showAttacks(playerTwo, gameboard);
+              checkWinner();
+            } else {
+              lastHitNum = lastHitNum - 10;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitTop = false;
+              showAttacks(playerTwo, gameboard);
+            }
+          } else {
+            hitTop = false;
+            this.aiAttack(
+              gameboard,
+              playerOne,
+              playerTwo,
+              showAttacks,
+              checkWinner
+            );
+          }
+        } else if (hitBot == true) {
+          if (
+            gameboard.board[lastHitNum + 9] !== undefined &&
+            !gameboard.board[lastHitNum + 9].hasBeenAttacked
+          ) {
+            if (gameboard.receiveAttack(lastHitNum + 10) == "Ship hit") {
+              lastHitNum = lastHitNum + 10;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitBot = true;
+              showAttacks(playerTwo, gameboard);
+              checkWinner();
+            } else {
+              lastHitNum = lastHitNum + 10;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitBot = false;
+              showAttacks(playerTwo, gameboard);
+            }
+          } else {
+            hitBot = false;
+            this.aiAttack(
+              gameboard,
+              playerOne,
+              playerTwo,
+              showAttacks,
+              checkWinner
+            );
+          }
+        } else {
+          if (
+            gameboard.board[firstHitNum - 2] !== undefined &&
+            gameboard.board[firstHitNum - 2].hasBeenAttacked == false
+          ) {
+            if (gameboard.receiveAttack(firstHitNum - 1) == "Ship hit") {
+              lastHitNum = firstHitNum - 1;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitLeft = true;
+              showAttacks(playerTwo, gameboard);
+              checkWinner();
+            } else {
+              gameboard.receiveAttack(firstHitNum - 1);
+              lastHitNum = firstHitNum - 1;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitLeft = false;
+              showAttacks(playerTwo, gameboard);
+            }
+          } else if (
+            gameboard.board[firstHitNum] !== undefined &&
+            gameboard.board[firstHitNum].hasBeenAttacked == false
+          ) {
+            if (gameboard.receiveAttack(firstHitNum + 1) == "Ship hit") {
+              lastHitNum = firstHitNum + 1;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitRight = true;
+              showAttacks(playerTwo, gameboard);
+              checkWinner();
+            } else {
+              gameboard.receiveAttack(firstHitNum + 1);
+              lastHitNum = firstHitNum + 1;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitRight = false;
+              showAttacks(playerTwo, gameboard);
+            }
+          } else if (
+            gameboard.board[firstHitNum + 9] !== undefined &&
+            gameboard.board[firstHitNum + 9].hasBeenAttacked == false
+          ) {
+            if (gameboard.receiveAttack(firstHitNum + 10) == "Ship hit") {
+              lastHitNum = firstHitNum + 10;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitBot = true;
+              showAttacks(playerTwo, gameboard);
+              checkWinner();
+            } else {
+              gameboard.receiveAttack(firstHitNum + 10);
+              lastHitNum = firstHitNum + 10;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitBot = false;
+              showAttacks(playerTwo, gameboard);
+            }
+          } else if (
+            gameboard.board[firstHitNum - 11] !== undefined &&
+            gameboard.board[firstHitNum - 11].hasBeenAttacked == false
+          ) {
+            if (gameboard.receiveAttack(firstHitNum - 10) == "Ship hit") {
+              lastHitNum = firstHitNum - 10;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitTop = true;
+              showAttacks(playerTwo, gameboard);
+              checkWinner();
+            } else {
+              gameboard.receiveAttack(firstHitNum - 10);
+              lastHitNum = firstHitNum - 10;
+              nums = nums.filter((num) => num !== lastHitNum);
+              hitTop = false;
+              showAttacks(playerTwo, gameboard);
+            }
+          } else {
+            let ranNum = Math.floor(Math.random() * nums.length);
+            if (gameboard.receiveAttack(nums[ranNum]) == "Ship hit") {
+              aiHit = true;
+              checkWinner();
+            }
+            firstHitNum = nums[ranNum];
+            lastHitNum = nums[ranNum];
+            nums.splice(ranNum, 1);
+            aiHit = false;
+            showAttacks(playerTwo, gameboard);
+          }
+        }
+      }
+      playerOne.isTurn = true;
+      playerTwo.isTurn = false;
     },
   };
 };
--- src/modules/ship.js
+++ src/modules/ship.js
@@ -20,7 +20,7 @@ const Ship = (length, name) => {
     },
     addedToBoard() {
       this.onBoard = true;
-    }
+    },
   };
 };
 
